5e55e88fd4bc7df59a31276577f824bb29164f9b
Afiq <afiqquraisyzulkarnain@gmail.com>
2023-06-15 09:11:34 +0800

    added cat command

---
+++ commands/cat.lua
@@ -0,0 +1,33 @@
+local cohttp = require("coro-http")
+local json = require("json")
+
+return {
+    name = "cat",
+    description = "Get random cat image.",
+    options = {},
+    cb = function(ia)
+        local res, body = cohttp.request("GET", "https://api.thecatapi.com/v1/images/search")
+
+        if res.code ~= 200 then
+            ia:reply(("Failed to GET: %d %s"):format(res.code, res.reason), true)
+            return
+        end
+
+        local cat_obj = assert(json.decode(body))
+        res, body = cohttp.request("GET", cat_obj[1].url)
+
+        if res.code ~= 200 then
+            ia:reply(("Failed to GET: %d %s"):format(res.code, res.reason), true)
+        end
+
+        local f = assert(io.open("cat.jpg", "w+b"))
+        f:write(body)
+        f:close()
+
+        local a_maxwell = ia.guild.emojis:find(function(e) return e.name == "Maxwell" end)
+        ia:reply({
+            content = ("Here's your cat image <a:Maxwell:%s>"):format(tostring(a_maxwell.id)),
+            file = "cat.jpg"
+        })
+    end
+}
