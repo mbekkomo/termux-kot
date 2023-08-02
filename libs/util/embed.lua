c1e69a01b9a3983273426e9371ce85dd2febb9b0
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2023-08-02 22:05:00 +0800

    style: format stylua

--- libs/util/embed.lua
+++ libs/util/embed.lua
@@ -3,11 +3,11 @@ local Object = require("core").Object
 ---@param tbl table
 ---@return table
 local function table_copy(tbl)
-	local buff = {}
-	for k, v in pairs(tbl) do
-		buff[k] = v
-	end
-	return buff
+    local buff = {}
+    for k, v in pairs(tbl) do
+        buff[k] = v
+    end
+    return buff
 end
 
 ---@class Embed : luvit.core.Object
@@ -16,50 +16,50 @@ local Embed = Object:extend()
 
 ---@return Embed
 function Embed:initialize()
-	self.embed = {}
-	return self
+    self.embed = {}
+    return self
 end
 
 ---@param t string
 ---@return Embed
 function Embed:setType(t)
-	self.embed.type = t
-	return self
+    self.embed.type = t
+    return self
 end
 
 ---@param title string
 ---@return Embed
 function Embed:setTitle(title)
-	self.embed.title = title
-	return self
+    self.embed.title = title
+    return self
 end
 
 ---@param desc string
 ---@return Embed
 function Embed:setDescription(desc)
-	self.embed.description = desc
-	return self
+    self.embed.description = desc
+    return self
 end
 
 ---@param url string
 ---@return Embed
 function Embed:setUrl(url)
-	self.embed.url = url
-	return self
+    self.embed.url = url
+    return self
 end
 
 ---@param iso string
 ---@return Embed
 function Embed:setTimestamp(iso)
-	self.embed.timestamp = iso
-	return self
+    self.embed.timestamp = iso
+    return self
 end
 
 ---@param color integer
 ---@return Embed
 function Embed:setColor(color)
-	self.embed.color = color
-	return self
+    self.embed.color = color
+    return self
 end
 
 ---@class Embed.Footer
@@ -70,8 +70,8 @@ end
 ---@param footer Embed.Footer
 ---@return Embed
 function Embed:setFooter(footer)
-	self.embed.footer = table_copy(footer)
-	return self
+    self.embed.footer = table_copy(footer)
+    return self
 end
 
 ---@class Embed.Image
@@ -83,8 +83,8 @@ end
 ---@param image Embed.Image
 ---@return Embed
 function Embed:setImage(image)
-	self.embed.image = table_copy(image)
-	return self
+    self.embed.image = table_copy(image)
+    return self
 end
 
 ---@class Embed.Thumbnail : Embed.Image
@@ -92,8 +92,8 @@ end
 ---@param thumbnail Embed.Thumbnail
 ---@return Embed
 function Embed:setThumbnail(thumbnail)
-	self.embed.thumbnail = table_copy(thumbnail)
-	return self
+    self.embed.thumbnail = table_copy(thumbnail)
+    return self
 end
 
 ---@class Embed.Video : Embed.Image
@@ -101,8 +101,8 @@ end
 ---@param video Embed.Video
 ---@return Embed
 function Embed:setVideo(video)
-	self.embed.video = table_copy(video)
-	return self
+    self.embed.video = table_copy(video)
+    return self
 end
 
 ---@class Embed.Provider
@@ -112,8 +112,8 @@ end
 ---@param provider Embed.Provider
 ---@return Embed
 function Embed:setProvider(provider)
-	self.embed.provider = table_copy(provider)
-	return self
+    self.embed.provider = table_copy(provider)
+    return self
 end
 
 ---@class Embed.Author : Embed.Footer
@@ -123,8 +123,8 @@ end
 ---@param author Embed.Author
 ---@return Embed
 function Embed:setAuthor(author)
-	self.embed.author = author
-	return self
+    self.embed.author = author
+    return self
 end
 
 ---@class Embed.Field
@@ -135,14 +135,14 @@ end
 ---@param field Embed.Field
 ---@return Embed
 function Embed:addField(field)
-	self.embed.fields = self.embed.fields or {}
-	self.embed.fields[#self.embed.fields + 1] = table_copy(field)
-	return self
+    self.embed.fields = self.embed.fields or {}
+    self.embed.fields[#self.embed.fields + 1] = table_copy(field)
+    return self
 end
 
 ---@return table
 function Embed:returnEmbed()
-	return self.embed
+    return self.embed
 end
 
 return Embed
